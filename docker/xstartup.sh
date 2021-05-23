508e97d816e9a24f18230f29501200f15573509d
me <st148204@stud.uni-stuttgart.de>
2021-05-24 00:44:04 +0200

    Fix unicode output of terminal emulator in container

--- docker/xstartup.sh
+++ docker/xstartup.sh
@@ -6,5 +6,6 @@ xsetroot -solid grey
 #x-window-manager &
 # Fix to make GNOME work
 export XKL_XMODMAP_DISABLE=1
+export LC_CTYPE=C.UTF-8 #Fix unicode output in uxterm
 /etc/X11/Xsession  &
 x-terminal-emulator -e "python3 /app/main.py"
