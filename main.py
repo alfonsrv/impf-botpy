540fdfdad72295a55ef79062fad39895ca1a1966
alfons <alfonsrv@protonmail.com>
2021-06-03 23:46:10 +0200

    Vermittlungscodes now require BIRTHDATE instead of AGE

--- main.py
+++ main.py
@@ -46,8 +46,7 @@ def instant_code() -> None:
     print('The bot is hard-coded to only request BioNTech + Moderna (18-60yo)')
     print('Should you request a vaccination for an older demographic or prefer another vaccine '
           'this method won\'t work for you.\n')
-    print('Please note: This feature is BROKEN(!) and just left here for reference. '
-          'Seems like they finally *actually* hired Software Engineers.')
+    print('Please note: This feature may break at any time – use it while you can!')
     print('If it breaks, please just fall back to using the browser instead.\n\n')
     zip_code = input('Enter the zip code of the location you require a Vermittlungscode for: ')
 
